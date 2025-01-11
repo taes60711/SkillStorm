dde73620b859a5f44b01c94b892126712e7f5c0a
“taes60711” <taes60711@gmail.com>
2025-01-11 08:57:11 +0800

    update

--- src/components/account/PwdForgotView.vue
+++ src/components/account/PwdForgotView.vue
@@ -1,26 +1,56 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 import PwdForgotViewModel from '../../view_models/account/pwd_forgot_view_model';
 import { ref } from 'vue';
   /// 創建View用的Model
   const viewModel = ref(new PwdForgotViewModel());
 
+  const router = useRouter();
+
+  /// 退出
+  const backToLoginPage = () => {
+    
+    /// 確認是否有編輯過
+    if(!viewModel.value.isEdit()){
+      router.back();
+    }else{
+      console.log(`正在編輯中 尚未給提示彈窗`);
+    }
+
+  }
+
 </script>
 
 <template>
   <div>
+    <input type="text" v-model="viewModel.emailController">
+    <p>信箱: {{ viewModel.emailController }}</p>
+    <p class="pwd_forgot_err" v-if="viewModel.emailIsEmpty">信箱不得為空</p>
+ 
     <input type="text" v-model="viewModel.codeController">
     <p>驗證碼: {{ viewModel.codeController }}</p>
-    <p v-if="viewModel.codeIsEmpty">驗證碼不得為空</p>
+    <p class="pwd_forgot_err" v-if="viewModel.codeIsEmpty">驗證碼不得為空</p>
+    
     <input type="text" v-model="viewModel.pwdController">
     <p>密碼: {{ viewModel.pwdController }}</p>
-    <p v-if="viewModel.pwdIsEmpty">密碼不得為空</p>
+    <p class="pwd_forgot_err" v-if="viewModel.pwdIsEmpty">密碼不得為空</p>
+    
     <input type="text" v-model="viewModel.newPwdController">
     <p>新密碼: {{ viewModel.newPwdController }}</p>
-    <p v-if="viewModel.newPwdIsEmpty">新密碼不得為空</p>
+    <p class="pwd_forgot_err" v-if="viewModel.newPwdIsEmpty">新密碼不得為空</p>
+   
+    <button @click="backToLoginPage">退出</button>
     <button @click="viewModel.send">送出</button>
+   
   </div>
 </template>
 
 <style scoped>
+  p{
+    color: #FFFFFF;
+  }
 
+  .pwd_forgot_err{
+    color: #f90202;
+  }
 </style>
--- src/view_models/account/pwd_forgot_view_model.ts
+++ src/view_models/account/pwd_forgot_view_model.ts
@@ -6,6 +6,9 @@ export default class PwdForgotViewModel {
     codeController = ref<string>(''); /// 驗證碼
     codeIsEmpty = ref<boolean>(false); /// 驗證碼 錯誤
 
+    emailController = ref<string>(''); /// 信箱
+    emailIsEmpty = ref<boolean>(false);  /// 信箱 錯誤
+
     pwdController = ref<string>(''); /// 密碼
     pwdIsEmpty = ref<boolean>(false);  /// 密碼 錯誤
 
@@ -18,11 +21,11 @@ export default class PwdForgotViewModel {
     send = () => {
         if (this.emptyCheck()) {
             console.log(
-                `Code: ${this.codeController.value} Pwd: ${this.pwdController.value} newPwd: ${this.newPwdController.value}`
+                `Code: ${this.codeController.value} Email: ${this.emailController.value} Pwd: ${this.pwdController.value} newPwd: ${this.newPwdController.value}`
             );
         } else {
             console.log(
-                `驗證碼為空？${this.codeIsEmpty} Pwd為空？${this.pwdIsEmpty} NewPwd為空？${this.newPwdIsEmpty}`
+                `驗證碼為空？${this.codeIsEmpty.value} Email為空？${this.emailIsEmpty.value} Pwd為空？${this.pwdIsEmpty.value} NewPwd為空？${this.newPwdIsEmpty.value}`
             );
         }
     };
@@ -42,6 +45,12 @@ export default class PwdForgotViewModel {
             isOK = false;
         }
 
+        /// 信箱
+        if (this.emailController.value.replaceAll(" ", "") === "") {
+            this.emailIsEmpty.value = true;
+            isOK = false;
+        }
+
         /// 密碼
         if (this.pwdController.value.replaceAll(" ", "") === "") {
             this.pwdIsEmpty.value = true;
@@ -56,9 +65,29 @@ export default class PwdForgotViewModel {
         return isOK;
     }
 
+    /**
+     * 重置所有錯誤
+     */
     private resetAllErr() {
         this.codeIsEmpty.value = false;
         this.pwdIsEmpty.value = false;
         this.newPwdIsEmpty.value = false;
+        this.emailIsEmpty.value = false;
     }
+
+    /**
+    * 確認是否編輯過
+    * @returns true:有編輯過 | false:正常
+    */
+    isEdit(): boolean {
+        let isEdit: boolean = false;
+        if (this.codeController.value != "" || this.emailController.value != ""
+            || this.pwdController.value != "" || this.newPwdController.value != "") {
+
+            isEdit = true;
+        }
+
+        return isEdit;
+    }
+
 }
